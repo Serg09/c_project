d50a72af75595e78ed19f5a4d3e6698895beeca7
Doug Knight <dougbknight@gmail.com>
2016-10-01 11:28:42 -0500

    WIP add angular

--- vendor/assets/javascripts/crowdscribed_1.0.0.js
+++ vendor/assets/javascripts/crowdscribed_1.0.0.js
@@ -2,19 +2,32 @@
   // set up the crowdscried object
   function registerStartup(fn) {
     if (typeof window.jQuery === 'undefined') {
+
+      console.log("no jQuery, cannot register startup script");
+
       return false
     }
     window.jQuery(fn);
+
+    console.log("registered startup script");
+
     return true;
   }
 
-  function tryRegister(fn, count, max) {
-    if(!registerStartup(fn)) {
-      if (count < max) {
-        window.setTimeout(function() {
-          tryRegister(fn, count + 1, max);
-        }, 500);
-      }
+  function tryFunction(fn, count, max) {
+    if(fn()) {
+
+      console.log("successfully called function");
+      console.log(fn);
+
+      return true;
+    } else if (count < max) {
+      window.setTimeout(function() {
+        tryFunction(fn, count + 1, max);
+      }, 500);
+    } else {
+      console.log("gave up in function");
+      console.log(fn);
     }
   }
 
@@ -22,36 +35,88 @@
   };
   Crowdscribed.prototype = {
     ready: function(fn) {
-      tryRegister(fn, 0, 3);
+      tryFunction(function() {
+        return registerStartup(fn);
+      }, 0, 10);
     }
   };
   window.crowdscribed = new Crowdscribed();
 
   // Initialize
-  function csInit() {
-    window.jQuery.fn.purchaseButton = function() {
+  function initializeJQuery() {
+    if (typeof window.jQuery === 'undefined') {
+      return false;
+    }
 
-      var a = $(document.createElement("A"));
-      a.text("Purchase");
-      a.attr("href", "#");
-      a.click(function() {
-        console.log("purchase button click, cha-ching");
-      });
-      this.append(a);
+    window.jQuery.fn.purchaseButton = function() {
+      if (window.angular === 'undefined') {
+        console.log("tried to insert directive before angular module is defined");
+      } else {
+        var div = $(document.createElement("DIV"));
+        div.attr("purchase-tile", true);
+        this.append(div);
+
+        console.log("appended purchase-tile div");
+      }
 
       return this;
     };
+    return true;
+  }
+
+  function initializeAngular() {
+    if (typeof window.angular === 'undefined') {
+      return false
+    }
+
+    console.log("define angular module");
+
+    // Angulr initialization
+    angular.module('crowdscribed')
+      .directive('purchaseTile', function() {
+        return {
+          template: '<a href="#">Purchase</a>'
+        }
+      })
+
+    console.log("registered angular directive 'purchaseTile'");
+    return true;
   }
 
   // ensure libraries are loaded
-  if(typeof window.jQuery === 'undefined') {
-    window.onload = function() {
-      var scriptElem = document.createElement("SCRIPT");
-      scriptElem.setAttribute("src", "https://code.jquery.com/jquery-3.1.1.slim.min.js");
-      document.body.appendChild(scriptElem);
-    }
-    tryRegister(csInit, 0, 3);
-  } else {
-    $(csInit());
+  function loadScript(src) {
+    var scriptElem = document.createElement("SCRIPT");
+    scriptElem.setAttribute("src", src);
+    document.body.appendChild(scriptElem);
+
+    console.log("inserted script tag for " + src);
   }
+
+  function registerLibraries() {
+    var libraries = [
+      {
+        src: "https://code.jquery.com/jquery-3.1.1.slim.min.js",
+        test: function() { return typeof window.jQuery !== 'undefined' }
+      },
+      {
+        src: "https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js",
+        test: function() { return typeof window.angular !== 'undefined' }
+      }
+    ];
+    for (var i = 0; i < libraries.length; i++) {
+      var library = libraries[i];
+      if (!library.test()) {
+        loadScript(library.src);
+      }
+    }
+  }
+
+  // TODO May need to handle client that already handles onload
+  window.onload = function() {
+    registerLibraries();
+  };
+
+  // initializers
+  tryFunction(initializeJQuery, 0, 3);
+  tryFunction(initializeAngular, 0, 3);
 })();
