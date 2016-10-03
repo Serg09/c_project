e964e4d23f6bd7e00cb513afccfdf8364d4adb58
Doug Knight <doug.knight@crowdscribed.com>
2016-10-03 17:46:53 -0500

    WIP working first API call

--- public/crowdscribed_1.0.0.js
+++ public/crowdscribed_1.0.0.js
@@ -4,10 +4,29 @@
     return;
   }
 
-  angular.module('crowdscribed', [])
-    .directive('purchaseTile', function() {
+  angular.module('crowdscribed', []).
+    directive('purchaseTile', function() {
       return {
-        template: '<div class="purchase-tile"><a href="#">Purchase</a></div>'
+        restrict: 'E',
+        templateUrl: 'purchase_tile.html'
       }
     })
+  .factory('cs', ['$http', function($http) {
+    this.getProduct = function(sku, callback) {
+      var url = 'http://localhost:3030/api/v1/products/' + sku;
+      $http.get(url, {
+        headers: {
+          'Authorization': 'Token token=fde100e5505140c5a93cede29321cd9c'
+        }
+      }).then(callback);
+    }
+    return this;
+  }])
+    .controller('purchaseTileController', ['$scope', 'cs', function($scope, cs) {
+      $scope.$watch('sku', function(newValue, oldValue) {
+        cs.getProduct(newValue, function(product) {
+          $scope.price = product.price;
+        });
+      });
+    }]);
 })();
