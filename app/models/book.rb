8a20924aff7252920af505952f7c938473aa0235
Doug Knight <dougbknight@gmail.com>
2016-11-24 15:45:16 -0600

    add Product model

--- app/models/book.rb
+++ app/models/book.rb
@@ -13,6 +13,7 @@ class Book < ActiveRecord::Base
   belongs_to :author, polymorphic: true
   has_many :versions, class_name: 'BookVersion', dependent: :destroy, autosave: true
   has_many :campaigns
+  has_many :products
   validates_presence_of :author_id, :author_type
 
   def active_campaign
